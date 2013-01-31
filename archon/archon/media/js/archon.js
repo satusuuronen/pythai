80be77118b7359fe15ffe6374feffd1e3afb464c
Satu Suuronen <satu.t.suuronen@gmail.com>
2013-01-31 10:15:27 +0200

    some clean-up

--- archon/archon/media/js/archon.js
+++ archon/archon/media/js/archon.js
@@ -28,54 +28,6 @@ var drawnBullets = [];
 
 var gameOver = false;
 
-
-function init() {
-    var c=document.getElementById("myCanvas");
-    var ctx=c.getContext("2d");
-    image.image.src = 'media/img/red_player.png';
-    imageMove.image.src = 'media/img/black_player.png';
-    imageBullet0.src = 'media/img/red_bullet.png';
-    imageBullet1.src = 'media/img/black_bullet.png';
-    images[0] = image;
-    images[1] = imageMove;
-    setInterval(update, 50);
-}
-
-
-function drawGrid() {
-    var c=document.getElementById("myCanvas");
-    var ctx=c.getContext("2d");
-    var gridH = canvasHeight / gridHeight; 
-    var gridW = canvasWidth / gridWidth;
-    for (var i = 0; i < (gridHeight + 1); i++) {
-        ctx.moveTo(0, i * gridH);
-        ctx.lineTo(canvasHeight, i * gridH);
-        ctx.stroke();
-    }
-    for (var i = 0; i < (gridWidth + 1); i++) {
-        ctx.moveTo(i * gridW,0);
-        ctx.lineTo(i * gridW,canvasWidth);
-        ctx.stroke();
-    }
-}
-
-function clear() {
-    var c=document.getElementById("myCanvas");
-    var ctx=c.getContext("2d");
-    for (index in images) {
-        var x = (images[index].x - 1) * (canvasWidth / gridWidth);
-        var y = (images[index].y - 1) * (canvasHeight / gridHeight);
-        ctx.clearRect(x, y, images[index].width, images[index].height);
-    }
-    while (drawnBullets.length > 0) {
-        var bullet = drawnBullets.pop();
-        var x = (bullet.x - 1) * (canvasWidth / gridWidth);
-        var y = (bullet.y - 1) * (canvasHeight / gridHeight);
-        ctx.clearRect(x, y, bullet.width, bullet.height);
-
-    }
-}
-
 function resetAndStartGame() {
     $.ajax({
         url: 'http://localhost:8000/game/reset',
@@ -87,6 +39,18 @@ function resetAndStartGame() {
             $("#popup").show();
         }
     });
+}
+
+function init() {
+    var c=document.getElementById("myCanvas");
+    var ctx=c.getContext("2d");
+    image.image.src = 'media/img/red_player.png';
+    imageMove.image.src = 'media/img/black_player.png';
+    imageBullet0.src = 'media/img/red_bullet.png';
+    imageBullet1.src = 'media/img/black_bullet.png';
+    images[0] = image;
+    images[1] = imageMove;
+    setInterval(update, 50);
 }
 
 function update() {
@@ -105,13 +69,7 @@ function updatePosition() {
             for (index in game.players) {   
                 var player = game.players[index]
                 var img = images[player.id];
-                //$("#playername" + index).html("Player " + (Number(index) + 1));
                 $("#progressbar" + index).progressbar({ value: player.energy});
-                if (img == undefined) {
-                    console.log(player);
-                    //alert("The player does not exist.");
-                    break;
-                }
                 img.x = player.x;
                 img.y = player.y;
                 draw(img);
@@ -133,12 +91,10 @@ function updatePosition() {
                 }
                 draw(bulletImage);
                 drawnBullets.push(bulletImage);
-            //console.log(bullet.x + ", " + bullet.y);
             }
             if (game.gameOver) {
                 gameOver = true;
                 console.log("Game over!");
-                //alert("Game Over!");
                 $("#popupcaption").html("Game over.");
                 if (game.winner == 0) {
                     $("#popuptext").html("You lost. The computer won.");
@@ -153,6 +109,21 @@ function updatePosition() {
 } 
 
 
+function clear() {
+    var c=document.getElementById("myCanvas");
+    var ctx=c.getContext("2d");
+    for (index in images) {
+        var x = (images[index].x - 1) * (canvasWidth / gridWidth);
+        var y = (images[index].y - 1) * (canvasHeight / gridHeight);
+        ctx.clearRect(x, y, images[index].width, images[index].height);
+    }
+    while (drawnBullets.length > 0) {
+        var bullet = drawnBullets.pop();
+        var x = (bullet.x - 1) * (canvasWidth / gridWidth);
+        var y = (bullet.y - 1) * (canvasHeight / gridHeight);
+        ctx.clearRect(x, y, bullet.width, bullet.height);
+    }
+}
 
 function draw(picture) {
     var c=document.getElementById("myCanvas");
@@ -185,16 +156,6 @@ function shoot(id) {
     });
 }
 
-
-function clearBullet() {
-    var c=document.getElementById("myCanvas");
-    var ctx=c.getContext("2d");
-    var x = (bullet.x - 1) * (canvasWidth / gridWidth);
-    var y = (bullet.y - 1) * (canvasHeight / gridHeight);
-    ctx.clearRect(x, y, bullet.width, bullet.height);
-}
-
-
 function doKeyDown(evt){
     var c=document.getElementById("myCanvas");
     var ctx=c.getContext("2d");
@@ -223,6 +184,25 @@ function doKeyUp(evt) {
     }
 }
 
+//for debugging purpose
+/*
+function drawGrid() {
+    var c=document.getElementById("myCanvas");
+    var ctx=c.getContext("2d");
+    var gridH = canvasHeight / gridHeight; 
+    var gridW = canvasWidth / gridWidth;
+    for (var i = 0; i < (gridHeight + 1); i++) {
+        ctx.moveTo(0, i * gridH);
+        ctx.lineTo(canvasHeight, i * gridH);
+        ctx.stroke();
+    }
+    for (var i = 0; i < (gridWidth + 1); i++) {
+        ctx.moveTo(i * gridW,0);
+        ctx.lineTo(i * gridW,canvasWidth);
+        ctx.stroke();
+    }
+}
+*/
 //The function color not in use. It was used to create the background image for the canvas.
 /*
 function color() {
